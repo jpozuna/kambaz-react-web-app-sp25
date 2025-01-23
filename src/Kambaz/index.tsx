dbaf4f4ea1eab6ff97ab61a114bd13e7fc5185f9
jpozuna <paulinoozuna.j@northeastern.edu>
2025-01-22 19:48:10 -0500

    adjusted landing page

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -14,7 +14,7 @@ export default function Kambaz() {
                     </td>
                     <td valign="top">
                         <Routes>
-                            <Route path="/" element={<Navigate to="Account" />} />
+                            <Route path="/" element={<Navigate to="Account/Signin" />} />
                             <Route path="Account/*" element={<Account />} />
                             <Route path="Dashboard" element={<Dashboard />} />
                             <Route path="Courses/:cid/*" element={<Courses />} />
