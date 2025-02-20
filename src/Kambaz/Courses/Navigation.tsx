b64f1f34631d95f86c601418f04dac214646c82c
jpozuna <paulinoozuna.j@northeastern.edu>
2025-02-20 00:07:34 -0500

    Fixing deploy errors part 3

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 
-export default function CourseNavigation({ links = [], cid, pathname }) {
-    return (
+export default function CourseNavigation( {
+    links = [],
+        cid = "" as string,
+        pathname = "" as string
+}) {
+return (
         <nav className="d-flex flex-column">
             {links.map((link, index) => {
                 const isActive = pathname.includes(`/${link}`);
